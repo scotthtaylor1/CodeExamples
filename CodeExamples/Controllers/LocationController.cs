6d68e8af6609f9398e916dc436f57f1d2f1be42e
scotthtaylor1 <scotthtaylor@yahoo.com>
2018-06-04 13:01:36 -0600

    Models / Lists added

---
+++ CodeExamples/Controllers/LocationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeExamples.Models;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeExamples.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET:  Location/ViewAllLocations
+        public IActionResult ViewAllLocations(int selectedCustomerID, int userID)
+        {
+            LocationModel model = new LocationModel();
+            List<Location> locations = Location.GetAllLocations(selectedCustomerID, userID);
+
+            if (locations != null)
+            {
+                model.Locations = locations;
+            }
+
+            return View(model);
+        }
+
+        public IActionResult GetLocationByID(int? locationID, int selectedCustomerID, int userID)
+        {
+            var location = Location.GetAllLocations(selectedCustomerID, userID).
+                Where(x => x.LocationID == locationID);
+
+            if (location != null)
+            {
+                LocationModel model = new Models.LocationModel();
+
+                foreach (var item in location)
+                {
+                    model.LocationName = item.LocationName;
+                    model.FullAddress = item.FullAddress;
+                    model.ParentLocationName = item.ParentLocationName;
+                }
+
+                return View(model);
+            }
+
+            return View(); //Should never reach this
+        }
+
+    }
+}
---
+++ CodeExamples/Models/ModelWithAnnotations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeExamples.Models
+{
+    public class ModelWithAnnotations
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Protected ID")]
+        public string ProtectedID { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
