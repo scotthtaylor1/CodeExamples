73a8bd95853235fde37bd01941117dac3fd7eff4
scotthtaylor1 <scotthtaylor@yahoo.com>
2018-06-05 10:10:41 -0600

    Some unit testing

---
+++ CodeExamplesTest/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeExamplesTest.Tests
+{
+    public class BaseClass
+    {
+        public Mock<ControllerContext> controllerContext;
+        public Mock<HttpContextBase> contextBase;
+
+        public BaseClass(string sessionVarName, string sessionValue)
+        {
+            controllerContext = new Mock<ControllerContext>();
+            contextBase = new Mock<HttpContextBase>();
+
+            controllerContext.Setup(x => x.HttpContext).Returns(contextBase.Object);
+            controllerContext.Setup(cc => cc.HttpContext.Session[sessionVarName]).Returns(sessionValue);
+        }
+        public BaseClass()
+        {
+        }
+    }
+}
---
+++ CodeExamplesTest/Controllers/LoginControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMExample.Controllers;
+using MVVMExample.Models;
+using MVVMExample.ViewModels;
+
+namespace CodeExamplesTest.Tests.Controllers
+{
+    [TestClass]
+    public class LoginControllerTest
+    {
+
+        [TestMethod]
+        public void Login()
+        {
+            // Arrange
+            LoginController loginController = new LoginController();
+            var returnUrl = "";
+
+            // Act
+            var actionResult = loginController.Login(returnUrl) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Model, typeof(LoginModel));
+        }
+
+        [TestMethod]
+        public void Login_UsernamePasswordEntered()
+        {
+            // Arrange
+            LoginController loginController = new LoginController();
+            LoginModel model = new LoginModel();
+            var returnUrl = "";
+            model.Username = "staylor";
+            model.ProtectedID = "123";
+            model.Password = "123";
+
+            // Act
+            var actionResult = loginController.Login(model, returnUrl) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Model, typeof(LoginModel));
+        }
+    }
+}
