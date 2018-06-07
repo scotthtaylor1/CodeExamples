57d18f5b3a59bdcafe43070c852e61cb39193270
scotthtaylor1 <scotthtaylor@yahoo.com>
2018-06-07 10:35:35 -0600

    Added http simulator
    HTTP Simulator mocks http requests for session variables.  Changed LoginControllerTest to use HTTP Simulator

--- CodeExamplesTest/Controllers/LoginControllerTest.cs
+++ CodeExamplesTest/Controllers/LoginControllerTest.cs
@@ -5,12 +5,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MVVMExample.Controllers;
 using MVVMExample.Models;
 using MVVMExample.ViewModels;
+using TestLibrary;
 
-namespace CodeExamplesTest.Tests.Controllers
+namespace MVVMExample.Tests.Controllers
 {
     [TestClass]
-    public class LoginControllerTest
+    public class LoginControllerTest : BaseClass
     {
+        public LoginControllerTest()
+        {
+        }
 
         [TestMethod]
         public void Login()
@@ -27,21 +31,34 @@ namespace CodeExamplesTest.Tests.Controllers
         }
 
         [TestMethod]
-        public void Login_UsernamePasswordEntered()
+        public void Login_TestDbReturn()
         {
             // Arrange
-            LoginController loginController = new LoginController();
+            var userID = "0";
+            LoginViewModel viewModel = new LoginViewModel();
             LoginModel model = new LoginModel();
-            var returnUrl = "";
             model.Username = "staylor";
             model.ProtectedID = "123";
             model.Password = "123";
 
             // Act
-            var actionResult = loginController.Login(model, returnUrl) as ViewResult;
+            using (new HttpSimulator("/", @"c:\inetpub\").SimulateRequest())
+            {
+                userID = viewModel.login(model).ToString();
+            }
 
-            // Assert
-            Assert.IsInstanceOfType(actionResult.Model, typeof(LoginModel));
+            //Assert
+            Assert.AreEqual("736", userID);
+        }
+
+        [TestMethod]
+        public void CanGetSetSession()
+        {
+            using (new HttpSimulator("/", @"c:\inetpub\").SimulateRequest())
+            {
+                HttpContext.Current.Session["Test"] = "Success";
+                Assert.AreEqual("Success", HttpContext.Current.Session["Test"], "Was not able to retrieve session variable.");
+            }
         }
     }
 }
